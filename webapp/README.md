5b860bada2bfcff632ed7731aea568101574e3d1
mhealth <33088494+codeseeking@users.noreply.github.com>
2021-03-15 16:18:54 +0800

    Delete webapp directory

--- webapp/README.md
+++
@@ -1,1 +0,0 @@
-Davinci Web Application
--- webapp/app/components/Avatar/types.ts
+++
@@ -1,9 +0,0 @@
-export type TSize = 'large' | 'default' | 'small'
-export interface IAvatarProps {
-  path?: string
-  size?: 'profile' | TSize
-  enlarge?: boolean
-  border?: boolean
-}
-
-export type TImageState = 'loading' | 'loaded' | 'loadFail'
